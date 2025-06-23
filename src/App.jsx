89652cc693db3e70dec24bfcca51402e3abdbab5
Pavithr jain <68775236+Pavithrj@users.noreply.github.com>
2025-06-23 06:38:26 +0000

    068

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Feature from "./components/Feature";
+import Workflow from "./components/Workflow";
+import Pricing from "./components/Pricing";
+import Testimonials from "./components/Testimonials";
+import Footer from "./components/Footer";
 
 function App() {
     return (
